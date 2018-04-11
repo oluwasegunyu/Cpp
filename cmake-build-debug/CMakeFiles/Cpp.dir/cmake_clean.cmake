dcd869306650a03b63c7c60f7398e599ac614e6c
yxy <yxy@yxy.com>
2018-04-11 18:03:00 +0800

    chapter 1

---
+++ cmake-build-debug/CMakeFiles/Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp.dir/Chapter1/main.cpp.o"
+  "Cpp.pdb"
+  "Cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
