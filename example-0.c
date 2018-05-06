092a61052fe09c819b5238b9bc958f026ddc9696
oluwasegunyu <charlay_yu@163.com>
2018-05-06 16:09:42 +0800

    第一个小程序

--- example-0.c
+++ example-0.c
@@ -1,25 +1,19 @@
 #include <gtk/gtk.h>
 
-static void activate (GtkApplication* app, gpointer user_data)
-{
-  GtkWidget *window;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show_all (window);
-}
-
 int main (int argc, char **argv)
 {
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
-
-  return status;
+  GtkWidget *window;
+  
+  /*初始化GTK+和所有的库*/
+  gtk_init(&argc, &argv);
+  
+  /*新建窗口*/
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Hello World");
+  gtk_widget_show(window);
+  
+  /*主循环控制*/
+  gtk_main();
+  return 0;
 }
 
